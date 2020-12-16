e8554c9ca54d5fcd7a4e9d54b1dbc6b0afcde0d8
Shane <slholloway@mun.ca>
2020-12-16 20:25:41 -0330

    Uploaded A4

---
+++ Assets/_Scripts/BulletBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BulletBehaviour : MonoBehaviour
+{
+    public float speed;
+    public Vector3 direction;
+    public float range;
+    public float radius;
+    public bool debug;
+    public bool isColliding;
+    public Vector3 collisionNormal;
+    public float penetration;
+    public float mass;
+
+    public BulletManager bulletManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isColliding = false;
+        radius = Mathf.Max(transform.localScale.x, transform.localScale.y, transform.localScale.z) * 0.5f;
+        bulletManager = FindObjectOfType<BulletManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+        if (speed < 0.01f)
+        {
+            speed = 4.0f;
+            bulletManager.ReturnBullet(this.gameObject);
+        }
+    }
+
+    private void _Move()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+        speed = speed * 0.9999999f;
+    }
+
+    private void _CheckBounds()
+    {
+        if (Vector3.Distance(transform.position, Vector3.zero) > range)
+        {
+            speed = 4.0f;
+            bulletManager.ReturnBullet(this.gameObject);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (debug)
+        {
+            Gizmos.color = Color.magenta;
+
+            Gizmos.DrawWireSphere(transform.position, radius);
+        }
+    }
+}
---
+++ Assets/_Scripts/CubeBehaviour.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Color = UnityEngine.Color;
+
+[System.Serializable]
+public class Contact : IEquatable<Contact>
+{
+    public CubeBehaviour cube;
+    public Vector3 face;
+    public float penetration;
+
+    public Contact(CubeBehaviour cube)
+    {
+        this.cube = cube;
+        face = Vector3.zero;
+        penetration = 0.0f;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null) return false;
+        Contact objAsContact = obj as Contact;
+        if (objAsContact == null) return false;
+        else return Equals(objAsContact);
+    }
+
+    public override int GetHashCode()
+    {
+        return this.cube.gameObject.GetInstanceID();
+    }
+
+    public bool Equals(Contact other)
+    {
+        if (other == null) return false;
+
+        return (
+            (this.cube.gameObject.name.Equals(other.cube.gameObject.name)) &&
+            (this.face == other.face) &&
+            (Mathf.Approximately(this.penetration, other.penetration))
+            );
+    }
+
+    public override string ToString()
+    {
+        return "Cube Name: " + cube.gameObject.name + " face: " + face.ToString() + " penetration: " + penetration;
+    }
+}
+
+
+[System.Serializable]
+public class CubeBehaviour : MonoBehaviour
+{
+    [Header("Cube Attributes")]
+    public Vector3 size;
+    public Vector3 max;
+    public Vector3 min;
+    public bool isColliding;
+    public bool debug;
+    public List<Contact> contacts;
+    public float mass;
+    public Vector3 velocity;
+
+    private MeshFilter meshFilter;
+    public Bounds bounds;
+    public bool isGrounded;
+    public bool isStatic;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        debug = false;
+        meshFilter = GetComponent<MeshFilter>();
+
+        bounds = meshFilter.mesh.bounds;
+        size = bounds.size;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        max = Vector3.Scale(bounds.max, transform.localScale) + transform.position;
+        min = Vector3.Scale(bounds.min, transform.localScale) + transform.position;
+        if (isGrounded)
+        {
+            velocity.x *= 0.95f;
+            velocity.y *= 0.95f;
+            velocity.z *= 0.95f;
+        }
+        transform.position += velocity;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (debug)
+        {
+            Gizmos.color = Color.magenta;
+
+            Gizmos.DrawWireCube(transform.position, Vector3.Scale(new Vector3(1.0f, 1.0f, 1.0f), transform.localScale));
+        }
+    }
+
+}
